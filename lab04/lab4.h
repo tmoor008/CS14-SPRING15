627128e3c1b70331385faf12478359bd59ea6329
tmoor008 <tmoor008@ucr.edu>
2015-05-03 21:03:58 +0000

    Final Lab4

--- lab04/lab4.h
+++ lab04/lab4.h
@@ -1,7 +1,15 @@
 //Name: Tia Moore
 //SID: 861144845
-//Date: 5/1/15
-//Approach:
+//Date: 5/3/15
+//Approach: In this lab I created three different traversal functions to
+//traverse through a specific tree and print it's value in a certain order.
+//For preorder and postorder traversal I used recursive calls and a specifically
+//placed cout statement to output them in the correct order. For the sorted
+//order I used a priority_queue class which allows me to store the values in
+//a vector. The sorted traversal uses recursive calls to store each set of
+//values into the vector using a push function. The push funtion not only stores
+//the values but sorts them into the correct ascending sum order. A print
+//function then iterates through the vector and prints its values. 
 
 #ifndef __LAB4_H__
 #define __LAB4_H__
@@ -14,53 +22,71 @@ using namespace std;
 
 typedef pair<int,int> Entry;
 
-class priority_queue 
+class priority_queue //class to hold values for in-order sorting
 {
     public:
         vector<Entry> entries;
-        Entry& front() { return entries.back(); }
-        void pop() { entries.pop_back(); }
+        Entry& front() { return entries.back(); } //returns the end values
+        void pop() { entries.pop_back(); } //pop function 
         void push(Entry e) 
         { 
             entries.push_back(e);
-            for (int i = entries.size() - 1; i != 0; --i) 
+            for (int i = entries.size()-1; i != 0; --i) //iterates through vec
             {
-              if (entries[i] <= entries[i - 1]) break;
-              swap(entries[i], entries[i - 1]);
+                if (entries[i].first + entries[i].second >= entries[i-1].first
+                    + entries[i-1].second) break; 
+                    //checks if the sum of the entry is greater than the sum
+                    //of the previous entry, and if so breaks out 
+                swap(entries[i], entries[i-1]); //otherwise it swaps to adjust
+            }
+        }
+        bool empty() //helper function to determine if the vector is empty
+        {
+            if (entries.empty()) return true;
+            else 
+            {
+                return false;
+            }
+        }
+        void print() //prints out the values for in-order
+        {
+            for (unsigned i = 0; i < entries.size(); ++i) //iterates
+            {
+                cout << entries[i].first << " " << entries[i].second << endl;
+                //outputs the first and second values
             }
         }
 };
 
 void preorder(int m, int n, int k)
 {
-    if (m + n > k) return;
+    if (m + n >= k) return; //base case to make sure sum is less than K
     
-    cout << m << " " << n << endl;
-    preorder(2 * m - n, m, k);
-    preorder(2 * m + n, m, k);
+    cout << m << " " << n << endl; //output at beginning for preorder
+    preorder(2 * m - n, m, k); //recursive calls that give the various
+    preorder(2 * m + n, m, k); //tree values
     preorder(m + 2 * n, n, k);
 }
 
 void postorder(int m, int n, int k)
 {
-    if (m + n > k) return;
+    if (m + n >= k) return; //base case to make sure sum is less than K
     
-    postorder(2 * m - n, m, k);
-    postorder(2 * m + n, m, k);
+    postorder(2 * m - n, m, k); //recursive calls that give the various
+    postorder(2 * m + n, m, k); //tree values
     postorder(m + 2 * n, n, k);
-    cout << m << " " << n << endl;
+    cout << m << " " << n << endl; //output at end for postorder
 }
 
 void inorder(int m, int n, int k, priority_queue &q)
 {
-    
-}
+    if (m + n >= k) return; //base case to make sure sum is less than K
 
-void inorder(int m, int n, int k)
-{
-    priority_queue q;
-    inorder(m, n, k, q);
-    //iterate over q and print
+    Entry T(m, n); //creates an Entry variable to hold m and n
+    q.push(T); //pushes the new Entry into the queue to sort
+    inorder(2 * m - n, m, k, q); //recursive calls that give the various
+    inorder(2 * m + n, m, k, q); //tree values
+    inorder(m + 2 * n, n, k, q);
 }
 
 #endif
--- lab04/main.cc
+++ lab04/main.cc
@@ -1,7 +1,15 @@
 //Name: Tia Moore
 //SID: 861144845
-//Date: 5/1/15
-//Approach:
+//Date: 5/3/15
+//Approach: In this lab I created three different traversal functions to
+//traverse through a specific tree and print it's value in a certain order.
+//For preorder and postorder traversal I used recursive calls and a specifically
+//placed cout statement to output them in the correct order. For the sorted
+//order I used a priority_queue class which allows me to store the values in
+//a vector. The sorted traversal uses recursive calls to store each set of
+//values into the vector using a push function. The push funtion not only stores
+//the values but sorts them into the correct ascending sum order. A print
+//function then iterates through the vector and prints its values. 
 
 #include <iostream>
 #include <cstdlib>
@@ -11,6 +19,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        cout << "Error: No value entered to run program. Aborting." << endl;
+        exit(-1);
+    }
+    
     int num = atoi(argv[1]);
     cout << "Pre-order" << endl;
     preorder(2, 1, num);
@@ -22,9 +36,69 @@ int main(int argc, char* argv[])
     postorder(3, 1, num);
     cout << endl;
     
-    // cout << "In-order" << endl;
-    // inorder(2, 1, num);
-    // inorder(3, 1, num);
+    cout << "Sorted" << endl;
+    priority_queue q;
+    inorder(2, 1, num, q);
+    inorder(3, 1, num, q);
+    q.print();
+    cout << endl;
+    
+    // various test cases
+    // cout << "Tests case where no nodes should be printed." << endl;
+    // int num1 = 0;
+    // cout << "Pre-order" << endl;
+    // preorder(2, 1, num1);
+    // preorder(3, 1, num1);
+    // cout << endl;
+    
+    // cout << "Post-order" << endl;
+    // postorder(2, 1, num1);
+    // postorder(3, 1, num1);
+    // cout << endl;
+    
+    // cout << "Sorted" << endl;
+    // priority_queue q1;
+    // inorder(2, 1, num1, q1);
+    // inorder(3, 1, num1, q1);
+    // q1.print();
+    // cout << endl;
+    
+    // cout << "Tests case where only (2, 1) should print" << endl;
+    // int num2 = 4;
+    // cout << "Pre-order" << endl;
+    // preorder(2, 1, num2);
+    // preorder(3, 1, num2);
+    // cout << endl;
+    
+    // cout << "Post-order" << endl;
+    // postorder(2, 1, num2);
+    // postorder(3, 1, num2);
+    // cout << endl;
+    
+    // cout << "Sorted" << endl;
+    // priority_queue q2;
+    // inorder(2, 1, num2, q2);
+    // inorder(3, 1, num2, q2);
+    // q2.print();
+    // cout << endl;
+    
+    // cout << "Tests normal case" << endl;
+    // int num3 = 7;
+    // cout << "Pre-order" << endl;
+    // preorder(2, 1, num3);
+    // preorder(3, 1, num3);
+    // cout << endl;
+    
+    // cout << "Post-order" << endl;
+    // postorder(2, 1, num3);
+    // postorder(3, 1, num3);
+    // cout << endl;
+    
+    // cout << "Sorted" << endl;
+    // priority_queue q3;
+    // inorder(2, 1, num3, q3);
+    // inorder(3, 1, num3, q3);
+    // q3.print();
     // cout << endl;
     
     return 0;
